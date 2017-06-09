1a8338419b5f87ab79ad53aa68dcac9383849795
tarun <tarun.softengg@gmail.com>
2017-06-09 08:30:03 +0530

    Create server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import winston from 'winston';
 winston.level = process.env.LOG_LEVEL || 'error'
 //winston.level = process.env.LOG_LEVEL || 'info'
 
-let app3 = express();
+let app4 = express();
 //---------------------------------------------//
 //@ invoke Middleware by passing express object
 //@ invoke routes and register to express instance
